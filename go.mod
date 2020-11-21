03d76048efccb3dd2a721ef722590e256f861012
jepsonyang <493088733@qq.com>
2020-11-21 09:01:24 +0800

    use testmod1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jepsonyang/testgomod
 
 go 1.14
+
+require github.com/jepsonyang/testmod1 v0.0.0-20201121005931-8334c4829168
--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jepsonyang/testmod1/jep"
+)
 
 func SayHello() {
 	fmt.Printf("hello jepson v1.1.0\n")
+	jep.Jep()
 }
